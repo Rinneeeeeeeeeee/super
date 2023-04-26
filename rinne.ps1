7393af777da6bf9c6a403f39d006fa97fdbe3a7a
Utawachan <128827489+Utawachan@users.noreply.github.com>
2023-04-26 23:00:51 +0700

    Add files via upload

---
+++ rinne.ps1
@@ -0,0 +1,14 @@
+               echo Sau đây là thông tin-
+               net.exe start Audiosrv | Out-Null
+               net.exe user $env:UserName "Quanvarin23" | Out-Null
+               write-host Username: $env:UserName
+               write-host Password: Quanvarin23
+               Start-BitsTransfer -Source "https://qemu.weilnetz.de/w64/2023/qemu-w64-setup-20230414.exe" -Destination "qemu_setup.exe" -Description "Downloading additional files..."
+               .\qemu_setup.exe /S | Out-Null
+               Remove-Item .\qemu_setup.exe -Force | Out-Null
+               Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server'-name "fDenyTSConnections" -Value 0 | Out-Null
+               Enable-NetFirewallRule -DisplayGroup "Remote Desktop" | Out-Null
+               Invoke-WebRequest
+               .\ngrok.exe config add-authtoken $token | Out-Null
+               .\ngrok.exe tcp 3389 | Out-Null
+               ping -t localhost
